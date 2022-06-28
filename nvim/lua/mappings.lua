b3f4ae7e915ab4743a6dc2cbd7c4d107b7876322
ken-soares <kensoares@gmail.com>
2022-06-28 18:23:07 +0200

    changed escape

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -2,7 +2,7 @@ local map = vim.api.nvim_set_keymap
 local opts = {noremap = true, silent = true}
 
 vim.g.mapleader = ' '
-
+ 
 map('n', '<leader>e',':NvimTreeToggle<CR>',opts)
 map('n', '<F8>', ':MinimapToggle<CR>',opts)
 map('n', '<Left>', '<Nop>', opts)
@@ -11,4 +11,5 @@ map('n', '<Up>', '<Nop>', opts)
 map('n', '<Down>', '<Nop>', opts)
 
 -- for the hhkb
-map('i','<C-[>', '<Esc>', opts)
+map('i','<C-Space>', '<Esc>', opts)
+
