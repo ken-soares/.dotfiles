78c249a2b1e5f626360f456fc84175952ac2494a
Kenneth <kensoares@gmail.com>
2022-11-19 19:34:31 +0100

    added config for rust analyzer

--- nvim/plugin/lspconfig.lua
+++ nvim/plugin/lspconfig.lua
@@ -65,6 +65,28 @@ protocol.CompletionItemKind = {
 
 nvim_lsp.pyright.setup({})
 
+nvim_lsp.rust_analyzer.setup({
+    on_attach = on_attach,
+    settings = {
+        ["rust-analyzer"] = {
+            imports = {
+                granularity = {
+                    group = "module",
+                },
+                prefix = "self",
+            },
+            cargo = {
+                buildScripts = {
+                    enable = true,
+                },
+            },
+            procMacro = {
+                enable = true
+            },
+        }
+    }
+})
+
 nvim_lsp.tsserver.setup {
     on_attach = on_attach,
     filetypes = { "typescript", "typescriptreact", "typescript.tsx" },
